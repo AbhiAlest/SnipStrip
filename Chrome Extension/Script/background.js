c52249c01568bd84f38ec19371244e559b42a198
AbhiAlest <128109884+AbhiAlest@users.noreply.github.com>
2023-05-10 16:16:02 -0500

    Update background.js

--- Chrome Extension/Script/background.js
+++ Chrome Extension/Script/background.js
@@ -1,6 +1,34 @@
-// Listen for messages from the content script
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   var selectedText = request.text;
-  // Perform the desired action with the selected text (e.g., copy to clipboard)
-  // Implement your logic here
+  
+  function copyToClipboard(text) {
+    var textarea = document.createElement("textarea");
+    document.body.appendChild(textarea);
+    textarea.value = text;
+    textarea.select();
+    document.execCommand("copy");
+    document.body.removeChild(textarea);
+  }
+  
+  function highlightText(text) {
+    var youtubeVideo = document.querySelector("video");
+    if (youtubeVideo) {
+      var videoText = youtubeVideo.innerText;
+      var highlightedText = videoText.replace(new RegExp(text, "gi"), function(match) {
+        return "<span style='background-color: yellow'>" + match + "</span>";
+      });
+      youtubeVideo.innerHTML = highlightedText;
+    }
+  }
+  
+  if (selectedText) {
+    var userChoice = confirm("Do you want to copy or highlight the selected text?");
+    if (userChoice) {
+      copyToClipboard(selectedText);
+      alert("Text copied to the clipboard!");
+    } else {
+      highlightText(selectedText);
+      alert("Text highlighted in the video!");
+    }
+  }
 });
