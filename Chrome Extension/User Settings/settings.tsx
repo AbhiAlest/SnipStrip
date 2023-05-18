75e53398293b02e6f11bda923b927f3f762956ac
AbhiAlest <128109884+AbhiAlest@users.noreply.github.com>
2023-05-18 14:00:59 -0500

    Update and rename settings.html to settings.tsx

---
+++ Chrome Extension/User Settings/settings.tsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+
+interface SettingsProps {}
+
+const Settings: React.FC<SettingsProps> = () => {
+  const [setting1, setSetting1] = useState(false);
+  const [setting2, setSetting2] = useState('');
+
+  const handleSave = () => {
+    // Save the settings
+  };
+
+  return (
+    <div>
+      <h1>Extension Settings</h1>
+
+      <div className="setting">
+        <label htmlFor="setting1">Setting 1:</label>
+        <input
+          type="checkbox"
+          id="setting1"
+          checked={setting1}
+          onChange={(e) => setSetting1(e.target.checked)}
+        />
+      </div>
+
+      <div className="setting">
+        <label htmlFor="setting2">Setting 2:</label>
+        <input
+          type="text"
+          id="setting2"
+          value={setting2}
+          onChange={(e) => setSetting2(e.target.value)}
+        />
+      </div>
+
+      <button onClick={handleSave}>Save</button>
+    </div>
+  );
+};
+
+export default Settings;
